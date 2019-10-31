656ec894bb6a9e4d1987d485fb5f1d25d94aa9fa
Sayantan Datta <std2048@gmail.com>
2019-10-31 18:11:36 -0400

    test0

--- correlationTest.py
+++ correlationTest.py
@@ -3,17 +3,21 @@ from fifl import *
 ######################################### Define Scenes #######################
 
 scenes = []
-alphas = [0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 1]
+#alphas = [0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 1]
+
+alphas = [0.1]
 
 # Scene 1 - Environment Light
 for alpha in alphas:
+    shift = -200
     scene = Scene(str(alpha))
     #reciever plane
     scene.append(Line(Point(-100,-75), Point(100, -75), material=Material(1.0, specularAlpha=alpha)))
     # Occluder
-    #scene.append(Line(Point(-75,125), Point(25, 125), material=Material(1.0, specularAlpha=0.5), mask=0xf0))
+    #scene.append(Line(Point(-50+shift,125), Point(50+shift, 125), material=Material(1.0, specularAlpha=0.5), mask=0xf0))
     #environment light
     scene.append(Light(radiance=0.4, mask=0xff))
+    scene.draw()
     scenes.append(scene)
     
 def generatePrimaryRays(nRays):
@@ -36,44 +40,44 @@ def generatePrimaryRays(nRays):
     return (primaryRays, angles)
 
 def shade(scene, primaryRay, nSecondaryRays):
-  (intersection, obj) = scene.intersect(primaryRay)
-  #primaryRay.draw()
+    (intersection, obj) = scene.intersect(primaryRay)
+    #primaryRay.draw()
 
-  if not intersection.hit:
-    return 0
+    if not intersection.hit:
+        return 0
   
-  if isinstance(obj, Light):
-    return obj.radiance
+    if isinstance(obj, Light):
+        return obj.radiance
 
-  (angles, weights) = sample("uniform", nSecondaryRays)
-  shadowRays = angleToRays(angles, intersection)
-  lightRays = angleToRays(angles, intersection, 0x0f)
+    (angles, weights) = sample("uniform", nSecondaryRays)
+    shadowRays = angleToRays(angles, intersection)
+    lightRays = angleToRays(angles, intersection, 0x0f)
  
-  shadowSamples = []
-  formFactorSamples = []
-  lightSamples = []
-  actualSamples = []
+    shadowSamples = []
+    formFactorSamples = []
+    lightSamples = []
+    actualSamples = []
 
-  for i in range(len(angles)):
-    formFactorSamples.append(obj.material.eval(intersection.normal, Vector(-primaryRay.pos[0], -primaryRay.pos[1]),  Vector(lightRays[i].pos[0], lightRays[i].pos[1])))
+    for i in range(len(angles)):
+        formFactorSamples.append(obj.material.eval(intersection.normal, Vector(-primaryRay.pos[0], -primaryRay.pos[1]),  Vector(lightRays[i].pos[0], lightRays[i].pos[1])))
     
-    (iSec, oSec) = scene.intersect(lightRays[i])
-    if isinstance(oSec, Light):
-        lightSamples.append(oSec.radiance * weights[i]) 
-    else:
-        lightSamples.append(0)
+        (iSec, oSec) = scene.intersect(lightRays[i])
+        if isinstance(oSec, Light):
+            lightSamples.append(oSec.radiance * weights[i]) 
+        else:
+            lightSamples.append(0)
 
-    (iSec, oSec) = scene.intersect(shadowRays[i])
-    #shadowRays[i].color = "red"
-    #shadowRays[i].draw()
-    if isinstance(oSec, Light):
-        shadowSamples.append(1)
-        actualSamples.append(formFactorSamples[i] * oSec.radiance * weights[i])
-    else:
-        shadowSamples.append(0)
-        actualSamples.append(0)
+        (iSec, oSec) = scene.intersect(shadowRays[i])
+        #shadowRays[i].color = "red"
+        #shadowRays[i].draw()
+        if isinstance(oSec, Light):
+            shadowSamples.append(1)
+            actualSamples.append(formFactorSamples[i] * oSec.radiance * weights[i])
+        else:
+            shadowSamples.append(0)
+            actualSamples.append(0)
 
-  return angles, shadowSamples, formFactorSamples, lightSamples, actualSamples
+    return angles, shadowSamples, formFactorSamples, lightSamples, actualSamples
 
 def computeCorrelations(shadowSamples, formFactorSamples, lightSamples, actualSamples):
     avgShadow = 0
@@ -90,8 +94,11 @@ def computeCorrelations(shadowSamples, formFactorSamples, lightSamples, actualSa
 
     correlationTerm = 0
     actualTerm = 0
+    correlationError = []
+
     for i in range(len(shadowSamples)):
         actualTerm = actualTerm + actualSamples[i]
+        correlationError.append((formFactorSamples[i] - avgFormFactor) * (shadowSamples[i] - avgShadow) * lightSamples[i])
         correlationTerm = correlationTerm + (formFactorSamples[i] - avgFormFactor) * (shadowSamples[i] - avgShadow) * lightSamples[i]
 
     approxTerm = avgShadow * avgFormFactor * lightNorm
@@ -99,11 +106,10 @@ def computeCorrelations(shadowSamples, formFactorSamples, lightSamples, actualSa
     #print(correlationTerm)
     #print(approxTerm)
     #print(actualTerm)
-
     if not (np.abs(correlationTerm + approxTerm - actualTerm) < 1e-10):
         print("Incorrect caluculations")
 
-    return np.abs(correlationTerm / actualTerm) * 100
+    return np.abs(correlationTerm / actualTerm) * 100, (correlationError / correlationTerm) * 100
 
 #computeCorrelations(shadowSamples, formFactorSamples, lightSamples, actualSamples)
 
@@ -116,22 +122,29 @@ def computeCorrelations(shadowSamples, formFactorSamples, lightSamples, actualSa
 # plt.show()
 
 errorVaryAlpha = []
-nPrimaryRays = 20
+nPrimaryRays = 5
 nSecondaryRays = 500
 for scene in scenes:
     errorVaryView = []
     index = 0
     (primaryRays, viewingAngles) = generatePrimaryRays(nPrimaryRays)
+    
     for primaryRay in primaryRays:
         (incidentAngles, shadowSamples, formFactorSamples, lightSamples, actualSamples)  = shade(scene, primaryRay, nSecondaryRays)
-        errorVaryView.append(computeCorrelations(shadowSamples, formFactorSamples, lightSamples, actualSamples))
-        #print(index)
+        (netError, correlationError) = computeCorrelations(shadowSamples, formFactorSamples, lightSamples, actualSamples)
+        errorVaryView.append(netError)
+        plt.plot(incidentAngles, correlationError, label=str(viewingAngles[index]))
+
+        print(netError)
         index = index + 1
+    
+    plt.legend()
+    plt.show()
     errorVaryAlpha.append(errorVaryView)
 
 (primaryRays, viewingAngles) = generatePrimaryRays(nPrimaryRays)
-for trajectory in errorVaryAlpha:
-    plt.plot(viewingAngles, trajectory)
+#for trajectory in errorVaryAlpha:
+#    plt.plot(viewingAngles, trajectory)
 
 plt.ylim(0, 50)
 plt.show()
