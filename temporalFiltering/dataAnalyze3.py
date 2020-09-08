bb246d77334f2fff9cd3aef187e7445973400924
Sayantan Datta <sayantan.datta@mail.mcgill.ca>
2020-09-08 17:15:58 -0400

    Updade

--- temporalFiltering/dataAnalyze3.py
+++ temporalFiltering/dataAnalyze3.py
@@ -58,7 +58,8 @@ mvAvgMc = mvAvgFilter(windowSize, mc)
 predictedErrorPlt = predictedError[2*windowSize + windowSize // 2:]
 actualErrorPlt = ref[2*windowSize - windowSize // 2: - windowSize // 2 + 1]- mvAvg[2*windowSize:]#ref[2*windowSize:]
 
-print(np.mean((actualErrorPlt[:- windowSize // 2 + 1] - predictedErrorPlt)**2)**0.5)
+trim = 2*windowSize
+print(np.mean(((actualErrorPlt[:- windowSize // 2 + 1] - predictedErrorPlt))[:-trim]**2)**0.5)
 
 plt.plot(predictedErrorPlt[:500], label="Predicted error")
 plt.plot(actualErrorPlt[:500], label="Actual error")
@@ -92,7 +93,7 @@ def mvAvgFilterAdv2(windowSz : int, data):
         filtered[i] = np.sum(window) / windowSz
 
         if i >= windowSz // 2:
-            filtered[i - windowSz // 2] += correction / windowSz
+            filtered[i - (windowSz // 2)] += correction / windowSz
 
     return filtered
 
@@ -101,38 +102,36 @@ def mvAvgFilterAdv2(windowSz : int, data):
 def mvAvgFilterAdv2Eff(windowSz : int, data):
     window = np.zeros(shape=windowSz)
     windowGrad = np.zeros(shape=windowSz)
+    windowFilteredNoGrad = np.zeros(shape=windowSz//2)
     filtered = np.zeros(shape=data.shape)
-    
-    windowSum = 0
+        
     gradSum = 0
     correctionLast = 0
     for i in range(data.shape[0]):
         inNew = data[i]
         gradOld = windowGrad[i % windowSz]
+        oldFiltered = windowFilteredNoGrad[i%(windowSz//2)]
         gradNew = (inNew - window[i % windowSz]) / windowSz
         windowGrad[i % windowSz] = gradNew
         window[i % windowSz] = inNew
         
         correction = correctionLast - ((windowSz // 2) + 1) * gradOld - (windowSz // 2) * gradNew + gradSum
-        windowSum += gradNew
+        windowFilteredNoGrad[i%(windowSz//2)] = windowFilteredNoGrad[(i + (windowSz//2) - 1) % (windowSz//2)] + gradNew
         correctionLast = correction
-        
-
-        filtered[i] = windowSum
+            
         gradSum += gradNew - gradOld
         if i >= windowSz // 2:
-            filtered[i - windowSz // 2] += correction / windowSz
-
+            filtered[i - windowSz // 2] = oldFiltered + correction / windowSz
+           
     return filtered
 
 mvAvg = mvAvgFilterAdv2(windowSize, n0)
 mvAvg2 = mvAvgFilterAdv2Eff(windowSize, n0)
 
 print("Efficient gradient error:")
-print(np.sqrt(np.mean((mvAvg - mvAvg2)[2*windowSize:]**2)))
+print(np.sqrt(np.mean((mvAvg - mvAvg2)[2*windowSize:-trim]**2)))
 
-
-plt.plot(mvAvg[windowSize:550], label="Coherent samples, variance:{0:0.6f}, MSE:{1:1.6f}".format(np.std(mvAvg[windowSize:] - ref[windowSize - windowSize // 2: - windowSize // 2 + 1]), np.mean((mvAvg[windowSize:] - ref[windowSize - windowSize // 2: - windowSize // 2 + 1])**2)**0.5) )
+plt.plot(mvAvg2[windowSize:550], label="Coherent samples, variance:{0:0.6f}, MSE:{1:1.6f}".format(np.std((mvAvg2[windowSize:] - ref[windowSize - windowSize // 2:- windowSize // 2 + 1])[:-trim]), np.mean((mvAvg2[windowSize:] - ref[windowSize - windowSize // 2:- windowSize // 2 + 1])[:-trim]**2)**0.5) )
 plt.plot(mvAvgMc[windowSize:550], label="MC samples, variance:{0:0.6f}, MSE:{1:1.6f}".format(np.std(mvAvgMc[windowSize:] - ref[windowSize - windowSize // 2: - windowSize // 2 + 1]), np.mean((mvAvgMc[windowSize:] - ref[windowSize - windowSize // 2: - windowSize // 2 + 1])**2)**0.5))
 plt.plot(ref[windowSize - windowSize // 2: 550 - windowSize // 2 + 1], label="Reference")
 plt.xlabel('time')
